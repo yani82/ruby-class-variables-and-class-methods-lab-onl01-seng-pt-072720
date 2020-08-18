82950e525b828073f09e7e9ba61c7f4aa1132a49
Yani Asari <yani.iman1@gmail.com>
2020-08-18 19:45:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,11 +34,10 @@ class Song
           genre_count[genre] += 1      
           else        
             genre_count[genre] = 1      
-            end    
-            end    
-            genre_count  
-            end
-  end 
+          end    
+        end   
+        genre_count  
+      end
   
   def self.artist_count
     artist_count = {}     
