04a25f3e4f2fc2f1383c0838d3c423ff98981334
Yani Asari <yani.iman1@gmail.com>
2020-08-18 19:45:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,15 +40,15 @@ class Song
       end
   
   def self.artist_count
-    artist_count = {}     
+    artist_count = {}   
     @@artists.each do |artist|     
       if artist_count[artist]    
         artist_count[artist] += 1       
         else       
           artist_count[artist] = 1     
-          end   
-          end    
-          artist_count   
-          end
-  end 
+        end   
+      end    
+      artist_count   
+  end
+ 
 end   
